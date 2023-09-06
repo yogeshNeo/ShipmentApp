6e9c357b8990e0d37d4b98cb97fae3b5f9fcd7c3
Yogesh Choudhary <yogesh.choudhary@neosoftmail.com>
2023-09-06 19:30:22 +0530

    api gateway changes

--- src/main/java/com/shipment/ShipmentApplication.java
+++ src/main/java/com/shipment/ShipmentApplication.java
@@ -3,9 +3,11 @@ package com.shipment;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @Slf4j
+@EnableDiscoveryClient
 @EnableFeignClients
 @SpringBootApplication
 public class ShipmentApplication {
